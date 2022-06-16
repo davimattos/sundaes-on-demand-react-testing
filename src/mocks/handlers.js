05f8b1e6897b69a7ba39c74237efce7d4afe8247
davimattostw <davi.mattos.tw@naturapay.net>
2022-06-15 22:24:46 -0300

    mock service worker setup

---
+++ src/mocks/handlers.js
@@ -0,0 +1,14 @@
+import { rest } from 'msw';
+
+export const handlers = [
+  rest.get('http://localhost:3030/scoops', (req, res, ctx) => res(ctx.status(200), ctx.json([
+    {
+      name: 'Vanilla',
+      imagePath: '/images/vanilla.png',
+    },
+    {
+      name: 'Chocolate',
+      imagePath: '/images/chocolate.png',
+    },
+  ]))),
+];
