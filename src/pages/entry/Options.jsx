eaf46e62007798d084b50c0ce63bb98f67284d78
davimattostw <davi.mattos.tw@naturapay.net>
2022-06-22 19:56:23 -0300

    test and code alert for server

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -2,21 +2,24 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Row } from 'react-bootstrap';
+import AlertBanner from '../common/AlertBanner';
 import ScoopOption from './ScoopOption';
 import ToppingOption from './ToppingOption';
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios.get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => {
-        // TODO: handle error response
-      });
+      .catch(() => setError(true));
   }, [optionType]);
 
-  // TODO: replace `null` with toopingOption when available
+  if (error) {
+    return <AlertBanner />;
+  }
+
   const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
 
   const optionItems = items.map((item) => (
---
+++ src/pages/entry/tests/Options.test.jsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import Options from '../Options';
+
+test('displays image for each scoop option from server', async () => {
+  render(<Options optionType="scoops" />);
+
+  const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
+  expect(scoopImages).toHaveLength(2);
+
+  // @ts-ignore
+  const allText = scoopImages.map((element) => element.alt);
+  expect(allText).toEqual(['Vanilla scoop', 'Chocolate scoop']);
+});
+
+test('displays image for each topping option from server', async () => {
+  render(<Options optionType="toppings" />);
+
+  const toppingImages = await screen.findAllByRole('img', { name: /topping$/i });
+  expect(toppingImages).toHaveLength(3);
+
+  // @ts-ignore
+  const allText = toppingImages.map((element) => element.alt);
+  expect(allText).toEqual(['M&Ms topping', 'Hot fudge topping', 'Cherries topping']);
+});
